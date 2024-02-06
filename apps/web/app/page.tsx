019aba9331522a2055243244b49289a1a5001ab0
Hari Chaitanya <chaitanyakodapaka@gmail.com>
2024-02-05 19:09:22 -0500

    tailwind added and created new app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,59 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
 
-function Gradient({
-	conic,
-	className,
-	small,
-}: {
-	small?: boolean;
-	conic?: boolean;
-	className?: string;
-}): JSX.Element {
+export default function Home() {
 	return (
-		<span
-			className={[
-				styles.gradient,
-				conic ? styles.glowConic : undefined,
-				small ? styles.gradientSmall : styles.gradientLarge,
-				className,
-			]
-				.filter(Boolean)
-				.join(" ")}
-		/>
-	);
-}
-
-const LINKS = [
-	{
-		title: "Docs",
-		href: "https://turbo.build/repo/docs",
-		description: "Find in-depth information about Turborepo features and API.",
-	},
-	{
-		title: "Learn",
-		href: "https://turbo.build/repo/docs/handbook",
-		description: "Learn more about monorepos with our handbook.",
-	},
-	{
-		title: "Templates",
-		href: "https://turbo.build/repo/docs/getting-started/from-example",
-		description: "Choose from over 15 examples and deploy with a single click.",
-	},
-	{
-		title: "Deploy",
-		href: "https://vercel.com/new",
-		description:
-			"Instantly deploy your Turborepo to a shareable URL with Vercel.",
-	},
-];
-
-export default function Page(): JSX.Element {
-	return (
-		<main className={styles.main}>
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>👋🏼 from Fit Viber</h1>
 		</main>
 	);
